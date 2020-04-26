82c11de60931990da17a5dbe6e859014f81f4b39
lcfjxzk <775126135@qq.com>
2020-04-26 22:34:22 +0800

    学到MiniCssExtractPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-
-var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = {
   //   mode: "production", //用户产品模式,代码少
@@ -19,12 +19,28 @@ module.exports = {
       title: "XDML - 写代码啦",
       template: "src/assets/index.html", //template,模板
     }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i, //如果发现了任何以.css结尾的文件名，就用"css-loader"把.css文件读到js里面，"style-loader"把style标签放到html里面。
-        use: ["style-loader", "css-loader"],
+
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "/public/path/to/",
+            },
+          },
+          "css-loader",
+        ],
+        // use: ["style-loader", "css-loader"],
       },
     ],
   },
